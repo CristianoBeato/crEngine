41c0d2d8dd9ba2140488f26cf9612254403a69c5
Beato <cristianobeato_dm@hotmail.com>
2025-10-09 19:09:30 -0300

    configure framework submodule

--- neo/idlib/precompiled.h
+++ neo/idlib/precompiled.h
@@ -71,41 +71,41 @@ If you have questions concerning this license or the applicable additional terms
 #define ID_TIME_T int64 // Signed because -1 means "File not found" and we don't want that to compare > than any other time
 
 // non-portable system services
-#include "../sys/sys_public.h"
+#include "sys/sys_public.h"
 
 // id lib
-#include "../idlib/Lib.h"
+#include "idlib/Lib.h"
 
 #include "sys/sys_filesystem.h"
 
 // framework
-#include "../framework/BuildVersion.h"
-#include "../framework/Licensee.h"
-#include "../framework/CmdSystem.h"
-#include "../framework/CVarSystem.h"
-#include "../framework/Common.h"
+#include "framework/BuildVersion.h"
+#include "framework/Licensee.h"
+#include "framework/CmdSystem.h"
+#include "framework/CVarSystem.h"
+#include "framework/Common.h"
 // DG: needed for idFile_InZip in File.h
-#include "../framework/Unzip.h"
+#include "framework/Unzip.h"
 // DG end
-#include "../framework/File.h"
-#include "../framework/File_Manifest.h"
-#include "../framework/File_SaveGame.h"
-#include "../framework/File_Resource.h"
-#include "../framework/FileSystem.h"
-#include "../framework/UsercmdGen.h"
-#include "../framework/Serializer.h"
-#include "../framework/PlayerProfile.h"
+#include "framework/File.h"
+#include "framework/File_Manifest.h"
+#include "framework/File_SaveGame.h"
+#include "framework/File_Resource.h"
+#include "framework/FileSystem.h"
+#include "framework/UsercmdGen.h"
+#include "framework/Serializer.h"
+#include "framework/PlayerProfile.h"
 
 // decls
-#include "../framework/TokenParser.h"
-#include "../framework/DeclManager.h"
-#include "../framework/DeclTable.h"
-#include "../framework/DeclSkin.h"
-#include "../framework/DeclEntityDef.h"
-#include "../framework/DeclFX.h"
-#include "../framework/DeclParticle.h"
-#include "../framework/DeclAF.h"
-#include "../framework/DeclPDA.h"
+#include "framework/TokenParser.h"
+#include "framework/DeclManager.h"
+#include "framework/DeclTable.h"
+#include "framework/DeclSkin.h"
+#include "framework/DeclEntityDef.h"
+#include "framework/DeclFX.h"
+#include "framework/DeclParticle.h"
+#include "framework/DeclAF.h"
+#include "framework/DeclPDA.h"
 
 // We have expression parsing and evaluation code in multiple places:
 // materials, sound shaders, and guis. We should unify them.
@@ -113,50 +113,50 @@ const int MAX_EXPRESSION_OPS = 4096;
 const int MAX_EXPRESSION_REGISTERS = 4096;
 
 // renderer
-#include "../renderer/OpenGL/qgl.h"
-#include "../renderer/Cinematic.h"
-#include "../renderer/Material.h"
-#include "../renderer/BufferObject.h"
-#include "../renderer/VertexCache.h"
-#include "../renderer/Model.h"
-#include "../renderer/ModelManager.h"
-#include "../renderer/RenderSystem.h"
-#include "../renderer/RenderWorld.h"
+#include "renderer/OpenGL/qgl.h"
+#include "renderer/Cinematic.h"
+#include "renderer/Material.h"
+#include "renderer/BufferObject.h"
+#include "renderer/VertexCache.h"
+#include "renderer/Model.h"
+#include "renderer/ModelManager.h"
+#include "renderer/RenderSystem.h"
+#include "renderer/RenderWorld.h"
 
 // sound engine
-#include "../sound/sound.h"
+#include "sound/sound.h"
 
 // user interfaces
-#include "../ui/ListGUI.h"
-#include "../ui/UserInterface.h"
+#include "framework/ui/ListGUI.h"
+#include "framework/ui/UserInterface.h"
 
-#include "../swf/SWF.h"
+#include "swf/SWF.h"
 
 // collision detection system
-#include "../cm/CollisionModel.h"
+#include "framework/cm/CollisionModel.h"
 
 // AAS files and manager
-#include "../tools/compilers/aas/AASFile.h"
-#include "../tools/compilers/aas/AASFileManager.h"
+#include "tools/compilers/aas/AASFile.h"
+#include "tools/compilers/aas/AASFileManager.h"
 
 // game
-#include "../d3xp/Game.h"
+#include "d3xp/Game.h"
 
 // Session / Network
-#include "../sys/LightweightCompression.h"
-#include "../sys/Snapshot.h"
-#include "../sys/PacketProcessor.h"
-#include "../sys/SnapshotProcessor.h"
+#include "sys/LightweightCompression.h"
+#include "sys/Snapshot.h"
+#include "sys/PacketProcessor.h"
+#include "sys/SnapshotProcessor.h"
 
-#include "../sys/sys_savegame.h"
-#include "../sys/sys_session_savegames.h"
-#include "../sys/sys_profile.h"
-#include "../sys/sys_localuser.h"
-#include "../sys/sys_signin.h"
-#include "../sys/sys_stats_misc.h"
-#include "../sys/sys_stats.h"
-#include "../sys/sys_session.h"
-#include "../sys/sys_achievements.h"
+#include "sys/sys_savegame.h"
+#include "sys/sys_session_savegames.h"
+#include "sys/sys_profile.h"
+#include "sys/sys_localuser.h"
+#include "sys/sys_signin.h"
+#include "sys/sys_stats_misc.h"
+#include "sys/sys_stats.h"
+#include "sys/sys_session.h"
+#include "sys/sys_achievements.h"
 
 //-----------------------------------------------------
 
@@ -164,24 +164,24 @@ const int MAX_EXPRESSION_REGISTERS = 4096;
 
 #ifdef GAME_DLL
 
-#include "../d3xp/Game_local.h"
+#include "d3xp/Game_local.h"
 
 #else
 
-#include "../framework/DemoChecksum.h"
+#include "framework/DemoChecksum.h"
 
 // framework
-#include "../framework/Compressor.h"
-#include "../framework/EventLoop.h"
-#include "../framework/KeyInput.h"
-#include "../framework/EditField.h"
-#include "../framework/DebugGraph.h"
-#include "../framework/Console.h"
-#include "../framework/DemoFile.h"
-#include "../framework/Common_dialog.h"
+#include "framework/Compressor.h"
+#include "framework/EventLoop.h"
+#include "framework/KeyInput.h"
+#include "framework/EditField.h"
+#include "framework/DebugGraph.h"
+#include "framework/Console.h"
+#include "framework/DemoFile.h"
+#include "framework/Common_dialog.h"
 
 // Compilers for map, model, video etc. processing.
-#include "../tools/compilers/compiler_public.h"
+#include "tools/compilers/compiler_public.h"
 
 #endif /* !GAME_DLL */
 
@@ -189,17 +189,17 @@ const int MAX_EXPRESSION_REGISTERS = 4096;
 
 //-----------------------------------------------------
 
-#include "../tools/edit_public.h"
+#include "tools/edit_public.h"
 
 // Files for dmap/aas/editors
-#include "../tools/compilers/dmap/idLibDmap/DmapDrawVert.h"
-#include "../tools/compilers/dmap/idLibDmap/DmapSurface.h"
-#include "../tools/compilers/dmap/idLibDmap/DmapSurface_Patch.h"
-#include "../tools/compilers/dmap/idLibDmap/DmapMapFile.h"
-#include "../tools/compilers/dmap/idLibDmap/DmapSimd.h"
-#include "../tools/compilers/dmap/renderer/DmapModel.h"
-#include "../tools/compilers/dmap/renderer/DmapModelManager.h"
-#include "../tools/compilers/dmap/renderer/DmapRenderWorld.h"
+#include "tools/compilers/dmap/idLibDmap/DmapDrawVert.h"
+#include "tools/compilers/dmap/idLibDmap/DmapSurface.h"
+#include "tools/compilers/dmap/idLibDmap/DmapSurface_Patch.h"
+#include "tools/compilers/dmap/idLibDmap/DmapMapFile.h"
+#include "tools/compilers/dmap/idLibDmap/DmapSimd.h"
+#include "tools/compilers/dmap/renderer/DmapModel.h"
+#include "tools/compilers/dmap/renderer/DmapModelManager.h"
+#include "tools/compilers/dmap/renderer/DmapRenderWorld.h"
 
 //-----------------------------------------------------
 
