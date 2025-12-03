c4f48c41ce4d5a8db5aa2e43190f53dc0879b2af
Beato <cristianobeato_dm@hotmail.com>
2025-12-03 15:40:08 -0300

    Implementation of logic base and abstraction classes for portable OpenGL and Vulkan implementations.

--- neo/renderer/GraphicsAPIWrapper.h
+++ neo/renderer/GraphicsAPIWrapper.h
@@ -54,9 +54,117 @@ class idRenderModelSurface;
 class idDeclRenderProg;
 class idRenderTexture;
 
-static const int MAX_OCCLUSION_QUERIES = 4096;
+inline constexpr int MAX_OCCLUSION_QUERIES = 4096;
 // returned by GL_GetDeferredQueryResult() when the query is from too long ago and the result is no longer available
-static const int OCCLUSION_QUERY_TOO_OLD				= -1;
+inline constexpr int OCCLUSION_QUERY_TOO_OLD				= -1;
+
+inline constexpr uint32_t MAX_UNIFORM_BLOCKS = 4086; // these are the maximum entities by draw call 
+inline constexpr uint32_t MAX_LIGHT_BLOCKS = 2048;
+inline constexpr uint32_t MAX_BINDING_SAMPLERS = 8192;
+
+struct float4
+{
+    float x = 0.0;
+    float y = 0.0; 
+    float z = 0.0;
+    float w = 0.0;
+};
+
+struct int4
+{
+    int x = 0;
+    int y = 0;
+    int z = 0;
+    int w = 0;
+};
+
+struct alignas( 16 ) vertexUniformBlock_t
+{
+    float4  rpLocalViewOrigin;
+    float4  rpViewOrigin;
+    float4  rpGlobalEyePos;
+
+    float4  rpBumpMatrixS;
+	float4  rpBumpMatrixT;
+    
+	float4  rpDiffuseMatrixS;
+	float4  rpDiffuseMatrixT;
+	
+	float4  rpSpecularMatrixS;
+	float4  rpSpecularMatrixT;
+	
+	float4  rpVertexColorModulate;
+	float4  rpVertexColorAdd;
+
+    float4  rpMVPmatrixX;
+	float4  rpMVPmatrixY;
+	float4  rpMVPmatrixZ;
+	float4  rpMVPmatrixW;
+	
+	float4  rpModelMatrixX;
+	float4  rpModelMatrixY;
+	float4  rpModelMatrixZ;
+	float4  rpModelMatrixW;
+	
+	float4  rpProjectionMatrixX;
+	float4  rpProjectionMatrixY;
+	float4  rpProjectionMatrixZ;
+	float4  rpProjectionMatrixW;
+	
+	float4  rpModelViewMatrixX;
+	float4  rpModelViewMatrixY;
+	float4  rpModelViewMatrixZ;
+	float4  rpModelViewMatrixW;
+	
+	float4  rpTextureMatrixS;
+	float4  rpTextureMatrixT;
+	
+	float4  rpTexGen0S;
+	float4  rpTexGen0T;
+	float4  rpTexGen0Q;
+	float4  rpTexGen0Enabled;
+	
+	float4  rpTexGen1S;
+	float4  rpTexGen1T;
+	float4  rpTexGen1Q;
+	float4  rpTexGen1Enabled;
+	
+	float4  rpWobbleSkyX;
+	float4  rpWobbleSkyY;
+	float4  rpWobbleSkyZ;
+	
+	float4  rpEnableSkinning;
+};
+
+struct alignas( 16 ) fragmentUniformBlock_t
+{
+    float4  rpScreenCorrectionFactor;
+    float4  rpWindowCoord;
+    float4  rpColor;
+    float4  rpOverbright;
+	float4  rpAlphaTest;
+};
+
+struct alignas( 16 ) lightUnifomBlock_t
+{   
+    float4  rpGlobalLightOrigin;
+    float4  rpLocalLightOrigin;
+    
+    float4  rpAmbientColor;
+    float4  rpDiffuseModifier;
+    float4  rpSpecularModifier;
+	
+    float4  rpLightProjectionS;
+	float4  rpLightProjectionT;
+	float4  rpLightProjectionQ;
+	
+    float4 rpLightFalloffS;
+
+};
+
+inline constexpr size_t FRAME_SSBO_VERT_SIZE = MAX_UNIFORM_BLOCKS * sizeof( vertexUniformBlock_t );
+inline constexpr size_t FRAME_SSBO_FRAG_SIZE = MAX_UNIFORM_BLOCKS * sizeof( fragmentUniformBlock_t );
+inline constexpr size_t FRAME_SSBO_LIGH_SIZE = MAX_UNIFORM_BLOCKS * sizeof( fragmentUniformBlock_t );
 
 /*
 ================================================================================================
@@ -176,5 +284,512 @@ void			GL_CheckErrors_Extended(const char* file, int line);
 
 wrapperStats_t	GL_GetCurrentStats();
 void			GL_ClearStats();
+
+// BEATO Begin:
+class crResourceState
+{
+public:
+    enum state_t : uint8_t
+    {
+        BUFFER_STATE_UNKNOW,
+        BUFFER_STATE_COPY_DESTINATION,  // buffer is a destination of a copy operation 
+        BUFFER_STATE_COPY_SOURCE,       // buffer is a source from a copy operation
+        BUFFER_STATE_USE_RENDER,        // buffer is used in a render operation
+        BUFFER_STATE_USE_COMPUTE,       // buffer is used in a compute operation
+        BUFFER_STATE_WRITE_COMPUTE      // buffer is a compute shader destination
+    };
+
+    crResourceState( void ): m_state( BUFFER_STATE_UNKNOW )
+    {
+    }
+
+
+    /// @brief Vulkan state transition 
+    /// @param in_state 
+    virtual void    StateTransition( const state_t in_state );
+    state_t     State( void ) const { return m_state; }
+
+protected:
+    state_t m_state;    // resource transition state
+};
+
+/// crBuffer
+/// @brief base class abstraction for common graphic buffer storage
+///
+class crBuffer : public crResourceState
+{
+public:
+    enum acess_t : uint8_t
+    {
+        BUFFER_ACCESS_NONE,
+        BUFFER_ACCESS_WRITE,
+        BUFFER_ACCESS_READ
+    };
+
+    enum type_t : uint8_t
+    {
+        BUFFER_TYPE_UNDEFINED,  // unknow buffer
+        BUFFER_TYPE_INDEX,      // primitive index buffer 
+        BUFFER_TYPE_VERTEX,     // vertex buffer
+        BUFFER_TYPE_SHADER,     // shader storage data
+        BUFFER_TYPE_COMMANDS,   // indirec draw comand buffer
+        BUFFER_TYPE_PIXEL       // pixel storage buffer
+    };
+
+    crBuffer( void );
+    ~crBuffer( void );
+
+    /// @brief create a fixed size buffer storage, data will remain maped till buffer are Destroyeds
+    /// @param in_type  the buffer storage usage type 
+    /// @param in_acess buffer acess for read or write 
+    /// @param in_size buffer size
+    /// @return true on sucess 
+    virtual bool    Create( const type_t in_type, const acess_t in_acess, const size_t in_size ) = 0;
+    
+    /// @brief try recreate the buffer and copy old content from old buffer
+    /// @param in_newSize new buffer size 
+    /// @return true on sucess
+    virtual bool    Resize( const size_t in_newSize ) = 0;
+    
+    /// @brief Destroy the buffer releasing the map and the memory 
+    virtual void    Destroy( void ) = 0;
+    
+    /// @brief Copy the content of the source buffer to this 
+    /// @param in_source the read source buffer 
+    /// @param in_srcOffset the read source offset 
+    /// @param in_dstOffset the write offset 
+    /// @param in_size the ammount of bytes to by copy
+    virtual void    CopyBuffer( const crBuffer* in_source, const uintptr_t in_srcOffset, const uintptr_t in_dstOffset, const size_t in_size ) const = 0;
+    
+    /// @brief flush buffer data ( send to device )
+    /// @param in_offset 
+    /// @param in_size 
+    virtual void    Flush( const uintptr_t in_offset, const size_t in_size ) const = 0;
+    
+    /// @brief Send data to buffer, perform a cpu copy of the data to the dst buffer 
+    /// @param in_data 
+    /// @param in_offset 
+    /// @param in_size 
+    virtual void    Upload( const void* in_data, const uintptr_t in_offset, const size_t in_size ) const;
+    
+    /// @brief 
+    /// @param in_data 
+    /// @param in_offset 
+    /// @param in_size 
+    virtual void    Download( void* in_data, const uintptr_t in_offset, const size_t in_size ) const;
+    
+    /// @brief 
+    /// @param  
+    /// @return 
+    virtual void*   Handle( void ) const = 0;
+    
+    type_t      Type( void ) const { return m_type; }
+    size_t      Size( void ) const { return m_size; }
+
+protected:
+    type_t  m_type;     // type of buffer data storage
+    acess_t m_acess;    // buffer acess type
+    size_t  m_size;     // bufer whole size
+    void*   m_data;     // pointer from buffer mapped data
+};
+
+class crTexture : public crResourceState
+{
+public:
+    enum type_t : uint8_t
+    {
+		TEXTURE_NONE,
+        TEXTURE_1D,
+        TEXTURE_2D,
+        TEXTURE_3D,
+        TEXTURE_CUBEMAP
+    };
+
+    enum format_t : uint8_t
+    {
+        TF_NONE,
+
+        // Red only
+        TF_R8I,
+        TF_R16I,
+
+        // Red green
+        TF_RG8I,
+        TF_RG16I,
+        // Uncompressed color 
+        TF_RGBA8,       // 32 bpp
+        TF_SRGBA8,      // 32 SRGB
+        TF_RGBA16,      // 64 bpp
+        TF_RGBA16F,
+        TF_RGBA32,
+        TF_RGBA32F,
+        TF_RGB565,  // 16 bpp
+        
+        // Compresed colors
+        TF_DXT1,
+        TF_DXT1_SRGB,
+        TF_DXT5,
+        TF_DXT5_SRGB,
+        
+        // Depth component 
+        TF_DEPTH16,
+        TF_DEPTH24,
+        TF_DEPTH32,
+
+        // Depth Stencil component 
+        TF_DEPTH24_STENCIL8,
+        TF_DEPTH32_STENCIL8,
+        TF_FORMAT_COUNT
+    };
+
+    struct dimensions_t
+    {
+        uint16_t    levels = 0;
+        uint16_t    layers = 0;
+        uint32_t    width = 0;
+        uint32_t    heigth = 0;
+        uint32_t    depth = 0;
+    };
+    
+    struct subImage_t
+    {
+        uint16_t    level;  // mipmap level
+        uint16_t    layer;  // layer of the multi texture
+        uint32_t    width;  // face width 
+        uint32_t    height; // face height
+        uint32_t    depth;  // face depth
+        uintptr_t   offset; // offset in texture buffer
+        size_t      size;   // pixel legenth
+    };
+
+    crTexture( void );
+    ~crTexture( void );
+
+    virtual bool    Create( const type_t in_type, const dimensions_t in_dimensions, const format_t in_format ) = 0;
+    virtual void    Destroy( void );
+    virtual void    SubImage( const uint32_t in_alignament, const idList<subImage_t> &in_subImages ) = 0;
+    virtual void*   Handler( void ) const;
+    const type_t    GetType( void ) const { return m_type; }
+    const format_t  GetFormat( void ) const { return m_format; }
+
+protected:
+    type_t          m_type;
+    format_t        m_format;
+    dimensions_t    m_dimensions;
+};
+
+class crSampler
+{
+public:
+    crSampler( void );
+    ~crSampler( void );
+
+    enum filter_t
+    {
+        FILTER_NEARES,
+        FILTER_LINEAR,
+        FILTER_BILINEAR,
+        FILTER_TRILINEAR,
+        FILTER_ANISOTROPIC2X,
+        FILTER_ANISOTROPIC4X,
+        FILTER_ANISOTROPIC8X,
+        FILTER_ANISOTROPIC16X
+    };
+
+    enum wrapping_t 
+    {
+        WRAP_NONE,
+        WRAP_REPEAT,
+        WRAP_MIRRORED,
+        WRAP_EDGE,
+        WRAP_BORDER        
+    };
+
+    virtual bool    Create( const filter_t in_filtering, const wrapping_t in_Swrap, const wrapping_t in_Twrap, const wrapping_t in_Rwrap ) = 0;
+    virtual void    Destroy( void ) = 0;
+    virtual void*   Handler( void ) const = 0;
+};
+
+class crFramebuffer
+{
+public:
+    struct Attachament_t
+    {
+        uint32_t    buffer;
+        uint32_t    colorAttachament;
+        uint32_t    depthStencil;
+        crTexture*  attachament;
+    };
+   
+    crFramebuffer( void );
+    ~crFramebuffer( void );
+
+    virtual bool Create(    const uint32_t in_bufferCount,
+                            const uint32_t in_width,
+                            const uint32_t in_height,
+                            const Attachament_t* in_attachaments, 
+                            const uint32_t in_numAttachaments );
+
+    virtual bool    Resize( const uint32_t in_width, const uint32_t in_height ) = 0;
+    
+    virtual void    Destroy( void ) = 0;
+
+    virtual void*   Handle( void ) const = 0;
+
+protected:
+    uint32_t m_width;
+    uint32_t m_height;
+};
+
+class crProgram
+{
+public:
+    enum type_t
+    {
+        PROG_VERTEX,
+        PROG_GEOMETRY,
+        PROG_FRAGMENT,
+        PROG_COMPUTE
+    };
+
+    crProgram( void );
+    ~crProgram( void );
+
+    virtual bool    Create( const type_t in_type, const void* in_source, const size_t in_size ) = 0;
+    virtual void    Destroy( void ) = 0;
+
+protected:
+    type_t  m_type;
+};
+
+/// @brief this is a wrapper/workarround to implemment pipelines 
+class crPipeline
+{
+public:
+    enum Face_t : uint8_t
+    {
+        FC_BACK,
+        FC_FRONT,
+        FC_TWO_FACES
+    };
+
+    enum PolygonMode_t : uint8_t
+    {
+        PM_POINT,   // render as point
+        PM_LINE,    // render as line
+        PM_FILL     // render as filled triangle
+    };
+
+    enum DepthFunc_t : uint8_t
+    {
+        DF_NONE,
+        DF_ALWAYS,  // aways pass 
+        DF_LESS,    // pass if less
+        DF_GREATER, // pass if grater
+        DF_EQUAL,   // pass if equal
+    };
+
+    enum ColorMask_t : uint8_t
+    {
+        CM_RED_MASK     = 1 << 1,
+        CM_GREEN_MASK   = 1 << 2,
+        CM_BLUE_MASK    = 1 << 3,
+        CM_ALPHA_MASK   = 1 << 4,
+    };
+
+    enum BlendSource_t : uint8_t
+    {
+        BLEND_SRC_ONE,
+        BLEND_SRC_ZERO,
+        BLEND_SRC_DST_COLOR,
+        BLEND_SRC_ONE_MINUS_DST_COLOR,
+        BLEND_SRC_SRC_ALPHA,
+        BLEND_SRC_ONE_MINUS_SRC_ALPHA,
+        BLEND_SRC_DST_ALPHA,
+        BLEND_SRC_ONE_MINUS_DST_ALPHA,
+    };
+
+    enum BlendDestination_t : uint8_t
+    {
+        BLEND_DST_ZERO,
+        BLEND_DST_ONE,
+        BLEND_DST_SRC_COLOR,
+        BLEND_DST_ONE_MINUS_SRC_COLOR,
+        BLEND_DST_SRC_ALPHA,
+        BLEND_DST_ONE_MINUS_SRC_ALPHA,
+        BLEND_DST_DST_ALPHA,
+        BLEND_DST_ONE_MINUS_DST_ALPHA,
+    };
+
+    enum BlendOperation_t : uint8_t
+    {
+        BLEND_OP_ADD,
+        BLEND_OP_SUB,
+        BLEND_OP_MIN,
+        BLEND_OP_MAX,
+    };
+
+    enum StencilFunc_t : uint8_t
+    {
+        STENCIL_FUNC_ALWAYS,
+        STENCIL_FUNC_LESS,
+        STENCIL_FUNC_LEQUAL,
+        STENCIL_FUNC_GREATER,
+        STENCIL_FUNC_GEQUAL,
+        STENCIL_FUNC_EQUAL,
+        STENCIL_FUNC_NOTEQUAL,
+        STENCIL_FUNC_NEVER
+    };
+
+    enum StencilOperation_t : uint8_t
+    {
+        STENCIL_OP_KEEP,
+        STENCIL_OP_ZERO,
+        STENCIL_OP_REPLACE,
+        STENCIL_OP_INCR,
+        STENCIL_OP_DECR,
+        STENCIL_OP_INVERT,
+        STENCIL_OP_INCR_WRAP,
+        STENCIL_OP_DECR_WRAP
+    };
+
+    enum AlphaFunc_t : uint8_t 
+    {
+        ALPHATEST_FUNC_ALWAYS,
+        ALPHATEST_FUNC_LESS,
+        ALPHATEST_FUNC_GREATER,
+        ALPHATEST_FUNC_EQUAL
+    };
+
+    struct PipelineInfo_t
+    {
+        Face_t                  faceCull;
+        Face_t                  polygonModeFace;
+        PolygonMode_t           polygonMode;
+        DepthFunc_t             depthFunc;
+        uint8_t                 colorMask;
+        BlendSource_t           blendSource;
+        BlendDestination_t      blendDestination;
+        BlendOperation_t        blendOperation;
+        Face_t                  stencilFace;
+        StencilOperation_t      stencilPass;
+        StencilOperation_t      stencilFail;
+        StencilOperation_t      stencilZFail;
+        AlphaFunc_t             alphaFunc;
+        uint32_t                numPrograms;
+        crProgram**             shaderPrograms;
+    };
+    
+    crPipeline( void );
+    ~crPipeline( void );
+
+    virtual void    Create( const PipelineInfo_t in_pipelineInfo );
+    virtual void    Destroy( void );
+
+protected:
+    PipelineInfo_t  m_pipelineConfiguration;
+};
+
+class crCommandBuffer
+{
+public:
+    crCommandBuffer( void );
+    ~crCommandBuffer( void );
+    virtual void    Begin( void ) = 0;
+    virtual void    End( void ) = 0;
+    virtual void    Sumit( void ) = 0;
+    virtual void    LineWidth( const float in_lineWidth ) const = 0;
+    virtual void    BindFrameBuffer( const crFramebuffer* in_framebuffef ) = 0;
+    virtual void    BindIndexBuffer( const crBuffer* in_buffer ) = 0;
+    virtual void    BindVertexBuffers( const crBuffer* in_buffer, uint32_t in_binding, const uintptr_t in_offsets, const size_t in_sizes, const size_t in_strides ) = 0;
+    virtual void    BindPipeline( const crPipeline* in_pipeline ) = 0;
+    virtual void    BeginRenderPass( void ) const = 0;
+    virtual void    EndRenderPass( void ) const = 0;
+    virtual void    Draw(  const uint32_t in_vertexCount, const uint32_t in_instanceCount, const uint32_t in_firstVertex, const uint32_t in_firstInstance ) const = 0;
+    virtual void    DrawIndexed( const uint32_t in_indexCount, const uint32_t in_instanceCount, const uint32_t in_firstIndex, const int32_t in_vertexOffset, const uint32_t in_firstInstance ) const = 0;
+    virtual void    Dispatch(  const uint32_t in_groupCountX, const uint32_t in_groupCountY, const uint32_t in_groupCountZ ) const = 0;
+    virtual void    Clear( bool color, bool depth, bool stencil, byte stencilValue, float r, float g, float b, float a ) = 0;
+    virtual void    PolygonOffset( const float scale, const float bias ) = 0;
+    virtual void    DepthBoundsTest( const float zmin, const float zmax ) = 0;
+    virtual void    Scissor( const int x, const int y, const int w, const int h ) const = 0;
+    virtual void    Viewport( const int x, const int y, const int w, const int h ) const = 0;
+
+    void    Scissor( const idScreenRect& rect ) const;
+    void    Viewport( const idScreenRect& rect ) const;
+    void	ViewportAndScissor( const idScreenRect& rect ) const;
+};
+
+ID_INLINE void crCommandBuffer::Scissor( const idScreenRect& rect ) const
+{
+	Scissor( rect.x1, rect.y1, rect.x2 - rect.x1 + 1, rect.y2 - rect.y1 + 1 );
+}
+
+ID_INLINE void	crCommandBuffer::Viewport( const idScreenRect& rect ) const
+{
+	Viewport( rect.x1, rect.y1, rect.x2 - rect.x1 + 1, rect.y2 - rect.y1 + 1 );
+}
+
+ID_INLINE void	crCommandBuffer::ViewportAndScissor( const idScreenRect& rect ) const
+{
+	Viewport( rect );
+	Scissor( rect );
+}
+
+class crSwapchain
+{    
+public:
+    crSwapchain( const uint32_t in_width, const uint32_t in_height );
+    ~crSwapchain( void );
+    virtual bool    Recreate( const uint32_t in_width, const uint32_t in_height ) = 0;
+    virtual void    AcquireImage( void ) = 0;
+    virtual void    PresentImage( void ) = 0;
+
+protected:
+    uint32_t    m_frame;        // current frame 
+    uint32_t    m_width; 
+    uint32_t    m_height;
+};
+
+class crBindlessTextureSlot 
+{
+public:
+    void    SetIndex( const uint32_t in_index ) { m_index = in_index; }
+    uint32_t GetIndex( void ) const { return m_index; }
+
+private:
+    uint32_t m_index;  // logic index 
+};
+
+/// @brief Store data block sto
+class crShaderStorage
+{
+public:
+    crShaderStorage( void );
+    ~crShaderStorage( void );
+    virtual void                    Submit( void );
+    virtual void                    SubmitLight( void );
+    virtual void                    Begin( void );
+    virtual void                    End( void );
+    virtual crBindlessTextureSlot*  BindTexture( const crTexture* in_texture, const crSampler* in_sampler ) = 0;
+    virtual void                    FreeSlot( crBindlessTextureSlot* &in_handle ) = 0;
+    virtual vertexUniformBlock_t*   GetvertexUniformBlock( void ) { return &m_vertexUniformBlock; };
+    virtual fragmentUniformBlock_t* GetFragmentUniformBlock( void ) { return &m_fragmentUniformBlock; };
+    virtual lightUnifomBlock_t*     GetLightUnifomBlock( void ) { return &m_lightUnifomBlock; }
+
+protected:
+    uint32_t                m_frame;
+    uint32_t                m_lastTextureIndex;
+    uint32_t                m_currentVBlock;
+    uint32_t                m_currentFSBlock;
+    uint32_t                m_currentLSBlock;
+    crBuffer*               m_VTSSBO;
+    crBuffer*               m_FGSSBO;
+    crBuffer*               m_LHSSBO;
+    vertexUniformBlock_t    m_vertexUniformBlock;
+    fragmentUniformBlock_t  m_fragmentUniformBlock;  
+    lightUnifomBlock_t      m_lightUnifomBlock;      
+    idList<uint32_t>        m_freeList;
+};
+// BEATO End
 
 #endif // !__GRAPHICSAPIWRAPPER_H__
