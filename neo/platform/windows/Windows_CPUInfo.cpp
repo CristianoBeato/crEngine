3029a261f1841173d06e3f46c98e0033dfd4205b
Cristiano Beato Dos Santos <Cristianobeato_dm@hotmail.com>
2026-09-21 14:46:33 -0300

    Fix window CPUInfo

--- neo/platform/windows/Windows_CPUInfo.cpp
+++ neo/platform/windows/Windows_CPUInfo.cpp
@@ -5,6 +5,36 @@
 
 #include <SDL3/SDL_cpuinfo.h>
 #include <windows.h>
+#include <intrin.h>
+
+constexpr int _REG_EAX = 0;
+constexpr int _REG_EBX = 1;
+constexpr int _REG_ECX = 2;
+constexpr int _REG_EDX = 3;
+
+static bool HasCMOV( void )
+{
+    int cpuInfo[4]{ 0, 0, 0, 0 };
+    
+    // get CPU feature bits
+    __cpuid(cpuInfo, 1 );
+
+    // bit 15 of EDX denotes CMOV existence
+	if ( cpuInfo[_REG_EDX] & ( 1 << 15 ) )
+        return true;
+
+    return false;
+}
+
+static bool HasDAZ( void )
+{
+    int cpuInfo[4]{ 0, 0, 0, 0 };
+    
+    // Read the MXCSR Mask
+    unsigned int mxcsr = _mm_getcsr();
+
+    return ( ( mxcsr & ( 1 << 6 ) ) == ( 1 << 6 ) );	// Return if the DAZ bit is set
+}
 
 crCPUInfo* crCPUInfo::Get( void )
 {
@@ -27,21 +57,45 @@ void crWindowsCPUInfo::Init(void)
     /// Retrieve the proc name
     GetProcessorName();
 
-    SDL_GetNumLogicalCPUCores();
-    SDL_GetCPUCacheLineSize();
-    SDL_HasAltiVec();
-    SDL_HasMMX();
-    SDL_HasSSE();
-    SDL_HasSSE2();
-    SDL_HasSSE3();
-    SDL_HasSSE41();
-    SDL_HasSSE42();
-    SDL_HasAVX();
-    SDL_HasAVX2();
-    SDL_HasAVX512F();
-    SDL_HasARMSIMD();
-    SDL_HasNEON();
+    m_cpuThreads = SDL_GetNumLogicalCPUCores();
+    m_cpuCacheLines = SDL_GetCPUCacheLineSize();
+    
+    if( SDL_HasAltiVec() )
+        CPUArchitecture |= CPUID_ALTIVEC;
 
+    if( SDL_HasMMX() )
+        CPUArchitecture |= CPUID_MMX;
+
+    if( SDL_HasSSE() )
+        CPUArchitecture |= CPUID_SSE;
+
+    if( SDL_HasSSE2() )
+        CPUArchitecture |= CPUID_SSE2;
+
+    if( SDL_HasSSE3() )
+        CPUArchitecture |= CPUID_SSE3;
+
+    if( SDL_HasSSE41() )
+        CPUArchitecture |= CPUID_SSE41;
+
+    if( SDL_HasSSE42() )
+        CPUArchitecture |= CPUID_SSE41;
+
+    if( SDL_HasAVX() )
+        CPUArchitecture |= CPUID_AVX;
+    if( SDL_HasAVX2() )
+        CPUArchitecture |= CPUID_AVX2;
+
+    if( SDL_HasAVX512F() )
+        CPUArchitecture |= CPUID_AVX512;
+
+    if( HasCMOV() )
+        CPUArchitecture |= CPUID_CMOV;
+
+    if( HasDAZ() )
+        CPUArchitecture |= CPUID_DAZ;
+
+    m_cpuIDFlags |= CPUArchitecture;
 }
 
 void crWindowsCPUInfo::GetProcessorName(void)
@@ -56,7 +110,7 @@ void crWindowsCPUInfo::GetProcessorName(void)
     if (openStatus != ERROR_SUCCESS) 
     {
         idLib::Warning( "Failed to open Win Register HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0" );
-        m_ProcessorName = idStr( "Unkow Processor" );
+        idStr::CopyString( m_ProcessorName, "Unkow Processor", 256 );
         return;
     }
 
@@ -69,7 +123,7 @@ void crWindowsCPUInfo::GetProcessorName(void)
     if (readStatus != ERROR_SUCCESS) 
     {
         idLib::Warning( "Failed to read Win Register \"ProcessorNameString\"" );
-        m_ProcessorName = idStr( "Unkow Processor" );
+        idStr::CopyString( m_ProcessorName, "Unkow Processor", 256 );
         return;
     }
 
@@ -83,5 +137,5 @@ void crWindowsCPUInfo::GetProcessorName(void)
     if ( std::strstr(buffer, "ARM") != nullptr || std::strstr(buffer, "arm") != nullptr )
         m_cpuIDFlags |= CPUID_ARM;
 
-    m_ProcessorName = buffer;
+    SDL_snprintf( m_ProcessorName, 256, buffer );
 }
